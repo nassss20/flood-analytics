3a8d8a5f351038b23f984dd7f6798c074729740c
onyo <149215886+nassss20@users.noreply.github.com>
2026-08-26 20:42:41 +0800

    chore: cleanup scratch files and old media

--- test_length.py
+++
@@ -1,7 +0,0 @@
-import requests
-for layer in [80, 81, 84]:
-    url = f"https://services5.arcgis.com/2ZRAaoTSJbQ20ceg/arcgis/rest/services/ROAD_ACCESSIBILITY_DUE_TO_FLOOD_WFL1/FeatureServer/{layer}?f=json"
-    data = requests.get(url).json()
-    for f in data.get('fields', []):
-        if f['name'].upper() == 'STATUS':
-            print(f"Layer {layer} Status length: {f.get('length')}")
